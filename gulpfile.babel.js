d382a20de599acb90095bec44b2b471f6ad93d70
Akinjide Bankole <r@akinjide.me>
2017-01-15 17:49:28 +0100

    refactor(gulp): change filename

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,10 +5,10 @@ import gulpLoadPlugins from 'gulp-load-plugins';
 const $ = gulpLoadPlugins();
 
 gulp.task('default', () =>
-  gulp.src('cookie.js')
+  gulp.src('cookiet.js')
     .pipe($.jshint())
     .pipe($.jshint.reporter('default'))
-    .pipe($.concat('cookie.min.js'))
+    .pipe($.concat('cookiet.min.js'))
     .pipe($.uglify())
     .pipe(gulp.dest(''))
 );
